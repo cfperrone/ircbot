e9b4cabc273ccc021cc08725b7b1bec3f95dcbda
Chris Perrone <chrisp510@gmail.com>
2014-09-19 21:18:08 +0000

    Broke out user-defined configuration into separate file. Cleaned up base

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports.config = {
+    server: '',
+    nick: 'hal',
+    prefix: '?',
+
+    // IRC Module Options
+    userName: 'hal',
+    realName: 'An IRC Bot',
+    port: 6667,
+    secure: true,
+    selfSigned: true,
+    certExpired: true,
+    channels: [
+    ],
+}
--- ircbot.js
+++ ircbot.js
@@ -2,37 +2,16 @@ var irc = require('irc'),
     path = require('path'),
     fs = require('fs');
 
-if (process.argv.length < 3) {
-    console.log("Usage: node ircbot.js <hostname>");
-    process.exit();
-}
-
-/* Configuration */
-var server = process.argv[2],
-    nick = 'hal',
-    prefix = '?',
-    dataDir = __dirname + '/data',
-    scriptDir = __dirname + '/scripts',
-    defaultCommand = "dolookup",
-    options = {
-        userName: nick,
-        realName: 'An IRC Bot',
-        port: 6667,
-        autoConnect: false,
-        channels: [
-        ],
-    };
-
-// Other globals
-this.scripts = [];
-this.commands = [];
+var config = getConfig(),
+    scripts = [],
+    commands = [];
 
 // Load scripts and instantiate commands
 loadModules();
 
 // Setup the server
-var client = new irc.Client(server, nick, options);
-// Instantiate hooks for various server events
+var client = new irc.Client(config.server, config.nick, config);
+// Crucial server events
 client.connect(function() {
     console.log("Connected!");
 });
@@ -41,17 +20,18 @@ client.addListener('error', function(message) {
 });
 client.addListener('message', function(from, to, message) {
     // Make sure the message has the server prefix, otherwise ignore
-    if (message.substr(0, prefix.length) != prefix) {
+    if (message.substr(0, config.prefix.length) != config.prefix) {
         return;
     }
 
     console.log(from + ' => ' + to + ': ' + message);
 
-    var replyto = (to == nick)?from:to;
-    var botmsg = message.substr(prefix.length);
+    var replyto = (to == config.nick)?from:to;
+    var botmsg = message.substr(config.prefix.length);
     handleRequest(from, to, botmsg, replyto);
 });
 
+// Deals with an incoming command to the bot
 function handleRequest(from, to, message, replyto) {
     var args = message.split(" "),
         first_arg = args[0],
@@ -59,7 +39,7 @@ function handleRequest(from, to, message, replyto) {
 
     // If the command doesn't exist fallback to a defined one
     if (!(first_arg in this.commands)) {
-        if (typeof defaultCommand != 'undefined' &&
+        if (typeof config.defaultCommand != 'undefined' &&
             defaultCommand in this.commands) {
             funcName = defaultCommand;
         } else {
@@ -76,15 +56,18 @@ function handleRequest(from, to, message, replyto) {
             raw_msg: message,
             args: args,
         };
+
     console.log("Calling command " + funcName);
     func(client, args);
 }
 
+// Instantiates all the modules in the scriptDir
+// and prepares them for execution by handleRequest
 function loadModules() {
     this.scripts = [];
     this.commands = [];
 
-    fs.readdir(scriptDir, function(err, files) {
+    fs.readdir(config.scriptDir, function(err, files) {
         if (err) {
             console.log("Error when loading files");
             process.exit();
@@ -94,7 +77,7 @@ function loadModules() {
             command_count = 0;
         for (var i = 0; i < files.length; i++) {
             var filename = files[i],
-                abspath = scriptDir + "/" + filename,
+                abspath = config.scriptDir + "/" + filename,
                 ext = path.extname(filename),
                 base = path.basename(filename, ext);
 
@@ -116,11 +99,12 @@ function loadModules() {
         console.log("Loaded " + script_count + " modules and " + command_count + " commands");
     });
 }
+
+// Reload the modules while the server is running
 this.reloadModules = function() {
-    // Reload the modules while the server is running
     console.log("Reloading the modules...");
 
-    // clear the module cache for all loaded modules
+    // Clear the module cache for all loaded modules
     for (var key in scripts) {
         var filename = scriptDir + '/' + key;
         delete require.cache[require.resolve(filename)];
@@ -129,11 +113,12 @@ this.reloadModules = function() {
     loadModules();
 }
 
-// Instantiate event to disconnect server on process exit
+// Event to disconnect server on process exit and reload modules
 process.on('SIGINT', cleanup);
 process.on('SIGHUP', this.reloadModules);
 process.on('SIGUSR2', cleanup);
 
+// Cleanly disconnects bot from the server
 function cleanup() {
     if (typeof client != 'undefined') {
         client.disconnect(function() {
@@ -148,4 +133,32 @@ function cleanup() {
             script.cleanup();
         }
     }
+}
+
+// Sets up configuration array combining user-defined
+// config with defaults
+function getConfig() {
+    // Configuration defaults
+    var config = {
+        nick: 'hal',
+        prefix: '?',
+        dataDir: __dirname + '/data',
+        scriptDir: __dirname + '/scripts',
+        defaultcommand: 'doLookup',
+
+        // IRC module options
+        userName: 'hal',
+        realName: 'An IRC Bot',
+        port: 6667,
+        autoConnect: false,
+        channels: [ ],
+    };
+
+    // Override default config with user-specified
+    var user_config = require('./config.js').config;
+    for (var attr in user_config) {
+        config[attr] = user_config[attr];
+    }
+
+    return config;
 }
